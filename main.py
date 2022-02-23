f5d5c4743464b749636a5b5d01b566f4521f08b5
Farrin Sofian <fsofian19@ku.edu.tr>
2022-02-24 01:35:53 +0300

    updated main.py

--- main.py
+++ main.py
@@ -4,11 +4,24 @@ from tqdm import tqdm
 
 inputs = input("Enter input directory and output directory in the following format: /path/to/input /path/to/output\n")
 input_dir, output_dir = inputs.split()
+species = os.listdir(input_dir)
 if not os.path.exists(output_dir):
     os.mkdir(output_dir)
 
-for file in tqdm(os.listdir(input_dir), desc = "converting"):
-    name = file.split('.')[0]
-    print('lame --decode ' + input_dir + '/' + file + ' '+ output_dir + '/' + name + '.wav')
-    os.system('lame --decode ' + input_dir + '/' + file + ' '+ output_dir + '/' + name + '.wav')
+   
+for bird in species: #list of species folder
+    if os.path.isfile(input_dir + '/' +bird):
+        continue
+    audios = os.listdir(input_dir + '/' + bird)
+    if "in_progress.txt" in audios:
+        audios.remove("in_progress.txt")
+    
+    if not bird in os.listdir(output_dir):
+        os.mkdir(output_dir + '/' + bird)
+    folders = tqdm(audios)
+    for audio in folders:
+        folders.set_description("Processing audios in %s" % bird)
+        name = audio.split('.')[0]
+        #print('lame --decode --quiet ' + input_dir + '/' +  bird + '/' + audio + ' '+ output_dir + '/' + bird + '/' + name + '.wav')
+        os.system('lame --decode --quiet ' + input_dir + '/' +  bird +  '/' + audio + ' '+ output_dir + '/' +  bird + '/' + name + '.wav')
     
